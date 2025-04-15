0552d618dfaa9bbbfbdd451942294e47f8541b25
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-15 11:10:37 -0700

    Create problem1.py

---
+++ Mod5/problem1.py
@@ -0,0 +1,13 @@
+'''
+#1. Prompt the user for a positive integer n. 
+Use a while loop to sum all the integers from 1 up to n.
+Print the final sum.
+'''
+
+n = int(input("Please give me a positive number: "))
+loop = 1
+sum = 0;
+while (loop < n):
+  sum += loop
+  loop = loop + 1
+print(sum)
