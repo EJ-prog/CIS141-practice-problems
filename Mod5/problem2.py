fece8b9f49b5ae55466df67be2c3b4814cf19c7f
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-15 11:35:10 -0700

    Update problem2.py
    Added the code.

--- Mod5/problem2.py
+++ Mod5/problem2.py
@@ -3,3 +3,7 @@
 Use a for loop to print each character on its own line.
 Convert each character to uppercase before printing.
 '''
+
+my_string = "Here is a sample string variable to loop through"
+for char in string:
+  print(char.upper())
