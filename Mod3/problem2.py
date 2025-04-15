5059926304a451528200b7357648cf92578c7bc1
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-15 09:29:40 -0700

    Create problem2.py

---
+++ Mod3/problem2.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age1 = int(input("What is your current age? ")
+age2 = age1 + 1
+print("Hello, " + name + "! You are " + age1 + " years old. Next year, you will be " + age2 + " years old.")
