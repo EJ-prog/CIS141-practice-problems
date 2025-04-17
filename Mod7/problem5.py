53f280d8dd17e7e2bbd261efedbf3469ee527e97
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-17 11:59:56 -0700

    Update problem5.py

--- Mod7/problem5.py
+++ Mod7/problem5.py
@@ -5,3 +5,10 @@ points and returns their new level based on these rules:
 * 100-199 XP → Level 2
 * 200+ XP → Level 3
 '''
+
+def level_up(experience):
+    if (experience >= 0 and experience <= 99):
+        return "Level 1"
+    elif (experience >= 100 and experience < 200):
+        return "Level 2"
+    return "Level 3"
