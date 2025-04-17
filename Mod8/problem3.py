7592ea3766e9de75d2a85444825bfc45fe2e237c
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-17 10:40:18 -0700

    Create problem3.py

---
+++ Mod8/problem3.py
@@ -0,0 +1,9 @@
+'''
+#3. Create a text file called song_lyrics.txt and copy the lyrics of a song into
+it. Write a Python program that:
+- Reads the file
+- Requests 5 inputs from the user: 5 words the user would like to count the frequency of
+- Counts how many times each word appears
+- Creates a dictionary of the words and their counts
+- Print the dictionary to the console
+'''
