4f906285934b1c8cde90b3d3f6b2d0586edb4a2c
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-17 10:06:29 -0700

    Update problem1.py
    Working code added.

--- Mod6/problem1.py
+++ Mod6/problem1.py
@@ -1,3 +1,13 @@
 '''
-#1. Create a list of integers (you get to pick!). Write code to iterate through the list and calculate the sum of all even numbers. Print the resulting sum.
+#1. Create a list of integers (you get to pick!). 
+Write code to iterate through the list and calculate the sum of all even numbers. 
+Print the resulting sum.
 '''
+
+numbers = [41, 29, 18, 48, 68, 35, 60, 73, 39, 84]
+even_nums = []
+for num in numbers:
+    if (num % 2 == 0):
+        even_nums.append(num)
+sum_of_even_nums = sum(even_nums)
+print(sum_of_even_nums)
