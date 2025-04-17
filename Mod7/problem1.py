8e517caa75c730e5c102e89b4346cf4cf042bf19
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-17 11:00:13 -0700

    Update problem1.py
    Only looks for lowercase vowels now.

--- Mod7/problem1.py
+++ Mod7/problem1.py
@@ -5,7 +5,6 @@ and returns the number of vowels (a, e, i, o, u) in it.
 
 def count_vowels(input):
     vowels = 0
-    input = input.lower()
     for words in input:
         if ("a" in words or "e" in words or "i" in words or "o" in words or "u" in words):
             vowels += 1
