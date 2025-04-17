bdf09691df714472c16a33b62a5df319dc9a486b
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-17 10:38:27 -0700

    Create problem5.py

---
+++ Mod7/problem5.py
@@ -0,0 +1,7 @@
+'''
+#5. Write a function called level_up(experience) that takes a player's experience
+points and returns their new level based on these rules:
+* 0-99 XP → Level 1
+* 100-199 XP → Level 2
+* 200+ XP → Level 3
+'''
