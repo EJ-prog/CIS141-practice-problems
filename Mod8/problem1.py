b845768d13291861eca9803ed7fa271fed704254
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-17 10:39:18 -0700

    Create problem1.py

---
+++ Mod8/problem1.py
@@ -0,0 +1,5 @@
+'''
+#1. Create a file called gardening_tips.txt and add at least 3 gardening tips to
+it. Write a Python script that reads a file gardening_tips.txt and prints
+out each tip one by one.
+'''
