c842607523d3c4c6ee4928cc74b6d45646c75ac8
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-17 10:20:29 -0700

    Update problem3.py
    Code added and comment reformatted.

--- Mod6/problem3.py
+++ Mod6/problem3.py
@@ -1,3 +1,12 @@
 '''
-#3. Create a list of strings. Write code to create a new list that includes only the strings longer than three characters. Print the resulting filtered list.
+#3. Create a list of strings. 
+Write code to create a new list that includes only the strings longer than three characters.
+Print the resulting filtered list.
 '''
+
+string_list = ["the", "fruit", "a", "vegetable", "several", "shops", "do", "sell", "these"]
+long_strings = []
+for strings in string_list:
+    if (len(strings) > 3):
+        long_strings.append(strings)
+print(long_strings)
