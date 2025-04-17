a1bd3e6de1fc1c67d07b8940aaddec3ba13b1f63
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-17 10:56:54 -0700

    Update problem1.py
    Function also accounts for uppercase vowels.

--- Mod7/problem1.py
+++ Mod7/problem1.py
@@ -2,3 +2,11 @@
 #1. Write a function called count_vowels(input) that takes a string
 and returns the number of vowels (a, e, i, o, u) in it.
 '''
+
+def count_vowels(input):
+    vowels = 0
+    input = input.lower()
+    for words in input:
+        if ("a" in words or "e" in words or "i" in words or "o" in words or "u" in words):
+            vowels += 1
+    return vowels
