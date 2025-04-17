4b002e41673c5b8a78c2905a821f2802e6262761
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-17 10:34:16 -0700

    Update problem5.py

--- Mod6/problem5.py
+++ Mod6/problem5.py
@@ -1,3 +1,13 @@
 '''
-#5. Create a list of integers. Use a loop to build a new list where each element is the square of the corresponding element in the original list. Print the new list.
+#5. Create a list of integers. 
+Use a loop to build a new list 
+where each element is the square 
+of the corresponding element in the
+original list. Print the new list.
 '''
+
+numbers = [-20, 19, -40, 37, -27, -17, -63, 39, 59, 18, -5, 23, 0]
+squared = []
+for nums in numbers:
+    squared.append(nums * nums)
+print(squared)
