b321dea994e15da397c6147bc416fbb037ed6602
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-17 11:17:51 -0700

    Update problem2.py
    Note to self: ask about ignoring spaces.

--- Mod7/problem2.py
+++ Mod7/problem2.py
@@ -3,3 +3,12 @@
 (reads the same forward and backward, ignoring case). The function should
 returns either True or False.
 '''
+#ask about ignoring spaces
+def is_palindrome(s):
+    s = s.lower()
+    i = 0
+    half = int(len(s)/2)
+    for i in range(half):
+        if (s[i] != s[len(s)-1-i]):
+            return False
+    return True
