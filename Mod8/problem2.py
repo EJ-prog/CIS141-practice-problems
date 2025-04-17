bd1998371bdf0f2bfe4911e2931a8384ae0cacb3
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-17 10:39:56 -0700

    Create problem2.py

---
+++ Mod8/problem2.py
@@ -0,0 +1,7 @@
+'''
+#2. Write a Python program that allows users to log their hiking trips. The program
+should:
+- Use a while loop to repeatedly ask for a hike name and distance in miles
+- Save each entry to hiking_log.txt (each hike on a new line)
+- When the user presses 0, exit the loop & print the contents of hiking_log.txt
+'''
