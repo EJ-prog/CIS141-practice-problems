e4ebb7c50d9755e9dddd3276da4f3219e7ceed6d
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-17 10:16:05 -0700

    Update problem2.py
    Code added to this program.

--- Mod6/problem2.py
+++ Mod6/problem2.py
@@ -1,3 +1,12 @@
 '''
-#2. Create a list of strings. Write code that counts how many times the word "Olympic" appears in the list, and then print the count.
+#2. Create a list of strings. 
+Write code that counts how many times the word "Olympic" appears in the list,
+and then print the count.
 '''
+
+count = 0
+string_list = ["Olympic College", "Olympic", "College", "Bremerton", "Poulsbo", "Shelton"]
+for strings in string_list:
+    if ("Olympic" in strings):
+        count += 1
+print(count)
