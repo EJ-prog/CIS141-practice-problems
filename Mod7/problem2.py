848e59e318bf44ebad1886594f78c175d7ac5db1
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-17 10:36:23 -0700

    Create problem2.py

---
+++ Mod7/problem2.py
@@ -0,0 +1,5 @@
+'''
+#2. Write a function called is_palindrome(s) that checks whether a string is a palindrome
+(reads the same forward and backward, ignoring case). The function should
+returns either True or False.
+'''
