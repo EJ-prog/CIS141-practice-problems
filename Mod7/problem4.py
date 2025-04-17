4da8ba030258bce331e7464f970381befaeff035
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-17 10:37:52 -0700

    Create problem4.py

---
+++ Mod7/problem4.py
@@ -0,0 +1,7 @@
+'''
+#4. Write a function called ferry_fare(age, vehicle) that calculates the Washington
+State Ferry fare based on age and whether the person has a vehicle. Assume the following rates:
+* Adults (19-64): $10 without a vehicle, $20 with a vehicle.
+* Seniors (65+): $5 without a vehicle, $15 with a vehicle.
+* Children (0-18): Free.
+'''
