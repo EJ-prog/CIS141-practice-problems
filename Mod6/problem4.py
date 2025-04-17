0c6deea0fe84f788ee2341f8e53388829fc9383b
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-04-17 10:29:30 -0700

    Update problem4.py
    Code to count the number of positive and negative numbers in a list has been added.

--- Mod6/problem4.py
+++ Mod6/problem4.py
@@ -1,3 +1,16 @@
 '''
-#4.  Create a list of integers. Write code that counts how many numbers are positive and how many are negative, then print both counts.
+#4.  Create a list of integers. 
+Write code that counts how many numbers are positive 
+and how many are negative, then print both counts.
 '''
+
+numbers = [-20, 19, -40, 37, -27, -17, -63, 39, 59, 18, -5, 23, 0]
+negatives = 0
+positives = 0
+for nums in numbers:
+    if (nums < 0):
+        negatives += 1
+    else:
+        positives += 1
+print(f"There are {negatives} negative numbers in the list.")
+print(f"There are {positives} positive numbers in the list.")
