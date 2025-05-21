d40b4f554576ee26f5352dd14d70ebad9d972a0f
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-05-21 11:31:53 -0700

    Update problem1.py
    Added a line to convert any input to lowercase and put a question next to it within a comment.

--- Mod7/problem1.py
+++ Mod7/problem1.py
@@ -5,6 +5,7 @@ and returns the number of vowels (a, e, i, o, u) in it.
 
 def count_vowels(input):
     vowels = 0
+    input = input.tolower() #Are we supposed to ignore case or just count lowercase vowels?
     for words in input:
         if ("a" in words or "e" in words or "i" in words or "o" in words or "u" in words):
             vowels += 1
