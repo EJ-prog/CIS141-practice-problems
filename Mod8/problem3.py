f570c2e4b165e93e94f8531f9f2d8505305a369b
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-05-27 12:55:08 -0700

    Update problem3.py
    Added code, which I wrote and tested in Visual Studio Code.

--- Mod8/problem3.py
+++ Mod8/problem3.py
@@ -7,3 +7,19 @@ it. Write a Python program that:
 - Creates a dictionary of the words and their counts
 - Print the dictionary to the console
 '''
+
+
+words = input("What five words would you like to count the frequency of? (separate them with ',') ")
+words_list = words.split(",")
+print(words_list)
+words_count = {}
+lyrics = open("song_lyrics.txt", "r")
+for line in lyrics:
+    for word in words_list:
+        if (word in line):
+            if (word in words_count):
+                words_count[word] += 1
+            else:
+                words_count[word] = 0
+lyrics.close()
+print(words_count)
