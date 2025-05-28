75b3ed7b415fe295db1bb3ff14ee44cb3173a1f7
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-05-27 17:01:55 -0700

    Update problem4.py
    Added code.

--- Mod8/problem4.py
+++ Mod8/problem4.py
@@ -3,3 +3,14 @@
 by commas. Write a program that reads the poll.txt file
 Count how many votes "yea" or "nay" received and print the results.
 '''
+
+yeas = 0
+nays = 0
+with open("poll.txt", "r") as file:
+    votes = file.read().split(", ")
+    for choice in votes:
+        if (choice.lower() == "yea"):
+            yeas += 1
+        elif (choice.lower() == "nay"):
+            nays += 1
+    print(f"There are {yeas} yeas and {nays} nays.")
