b42438ec1cb82ed3f21c38000006826eb79fb31e
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2025-06-03 10:05:32 -0700

    Update problem3.py
    Now counts the number of times a word occurs in one line. Also counts words regardless of case.

--- Mod8/problem3.py
+++ Mod8/problem3.py
@@ -10,16 +10,18 @@ it. Write a Python program that:
 
 
 words = input("What five words would you like to count the frequency of? (separate them with ',') ")
-words_list = words.split(",")
+words_list = words.lower().split(",")
 print(words_list)
 words_count = {}
 lyrics = open("song_lyrics.txt", "r")
 for line in lyrics:
-    for word in words_list:
-        if (word in line):
+    lyric_lines = line.lower().strip().split(" ")
+#    print(lyric_lines)
+    for word in lyric_lines:
+        if (word in words_list):
             if (word in words_count):
                 words_count[word] += 1
             else:
-                words_count[word] = 0
+                words_count[word] = 1
 lyrics.close()
 print(words_count)
